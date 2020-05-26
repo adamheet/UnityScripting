bec7787820f44bb3eb6e7d61a41e16879dea022e
adamheet <aheet@nd.edu>
2020-05-26 16:09:27 -0400

    uploading Udemy Intermediate environments
    didn't have a build.bc file anyway.

---
+++ UdemyIntermediate/Assets/Scripts/Car.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Car 
+{
+    //attributes are variables
+    public float speed;
+    public int topspeed;
+    public string color;
+    public float gears;
+
+    public Car()
+    {
+        Debug.Log("Car() constructor called");
+    }
+
+    public Car(float speed,int topspeed,string color)
+    {
+        this.speed = speed;
+        this.topspeed = topspeed;
+        this.color = color;
+        Debug.Log("Car(speed,int,string) constructor called");
+    }
+
+
+
+    //behaviors are methods
+    void move()
+    {
+        Debug.Log("moving");
+    }
+
+    void brake()
+    {
+        Debug.Log("braking");
+    }
+
+    public void printspeed()
+    {
+        Debug.Log(speed);
+    }
+
+    public void printDetails()
+    {
+        Debug.Log("speed is = " + speed);
+        Debug.Log("topspeed is = " + topspeed);
+        Debug.Log("color is  = " + color);
+        Debug.Log("gears is = " + gears);
+    }
+}
---
+++ UdemyIntermediate/Assets/Scripts/ClassTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Car Jeep;
+    public Car Kia;
+
+    void Start()
+    {
+        /* Car Honda = new Car();
+         Honda.speed = 10.5f;
+         Honda.topspeed = 30;
+         Honda.color = "Red";
+
+         Car Ford = new Car();
+         Ford.speed = 5f;
+         Ford.topspeed = 15;
+         Ford.color = "Green";
+
+         Ford.printspeed();
+         Honda.printspeed();
+
+         Car Kia = new Car(15.5f, 80, "Grey");
+         */
+
+        Jeep.printDetails();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

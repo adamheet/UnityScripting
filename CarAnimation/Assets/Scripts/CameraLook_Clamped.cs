70028b51b58bdea7873e75af818a6bed7d888763
adamheet <aheet@nd.edu>
2020-05-26 11:39:14 -0400

    Upload of CarAnimation
    Environment includes clockwise and counterclockwise traffic

---
+++ CarAnimation/Assets/Scripts/CameraLook_Clamped.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook_Clamped : MonoBehaviour
+{
+    public float pitchMultiplier;
+    public float minAngle;
+    public float maxAngle;
+    public float yawMultiplier;
+    float pitch;
+    float yaw;
+    public float minYaw;
+    public float maxYaw;
+    private Quaternion camRotation;
+
+    private void Start()
+    {
+        camRotation = transform.localRotation;
+    }
+
+    void Update()
+    {
+        pitch += Input.GetAxis("Mouse Y") * -1 * pitchMultiplier; //look up and down
+        yaw += Input.GetAxis("Mouse X") * 1 * yawMultiplier; //look left and right
+
+        pitch = Mathf.Clamp(pitch, minAngle, maxAngle);
+        yaw = Mathf.Clamp(yaw, minYaw, maxYaw);
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, camRotation.z);
+
+
+
+
+
+        // pitch += Input.GetAxis("Mouse Y") * 10 * pitchMultiplier * Time.deltaTime;
+        // Debug.Log (pitch);
+        //transform.Rotate(pitch , 0, 0);
+
+        //Clamp(float value, float min, float max)
+        // Mathf.Clamp(eulerpitch, minAngle, maxAngle);
+
+
+    }
+}
---
+++ CarAnimation/Assets/Scripts/createCar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCar : MonoBehaviour
+{
+    public GameObject vehicle;
+    public Transform origin;
+    public string direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(direction))
+        {
+            Instantiate(vehicle, origin);
+        }
+    }
+}

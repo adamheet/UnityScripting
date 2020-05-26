784a83dd8569b7f5a6d797f443bb288f319f96db
adamheet <aheet@nd.edu>
2020-05-26 16:13:45 -0400

    upload of NavMeshMechanics
    didn't include a build.bc file

---
+++ NavMeshMechanics/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+
+    bool open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (open)
+            {
+                transform.Translate(-3, 0, -5);
+                open = false;
+            }
+            else
+            {
+                transform.Translate(3, 0, 5);
+                open = true;
+            }
+        }
+    }
+}
